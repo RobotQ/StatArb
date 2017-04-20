90df0f7ccedc8177c0f29ad0e7dba958dd044984
RobotQ <qiufang@umich.edu>
2017-04-19 20:45:56 -0400

    add shibor

--- MarginTradingAnalysis.py
+++ MarginTradingAnalysis.py
@@ -1,7 +1,13 @@
 # import some data from tushare
 import tushare as ts
 import matplotlib.pyplot as plt
+import pandas as pd
 
-df = ts.shibor_data(2017)
-plt.plot(df["ON"])
+res = []
+years = range(2006, 2017)
+for year in years:
+    res.append(ts.shibor_data(year))
+
+df = pd.concat(res)
+plt.plot(df["date"], df["1W"])
 
